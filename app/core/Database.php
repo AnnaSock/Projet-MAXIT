546328572754fee6b03cbb1a3392588c3f13b8b5
AnnaSock <annasock542@gmail.com>
2025-07-27 18:49:14 +0000

    railway

--- app/core/Database.php
+++ app/core/Database.php
@@ -9,10 +9,11 @@ class Database{
 
         }
 
+        
 
         public static function getInstance(){
                 if(self::$pdo===null){
-                     self::$pdo= new PDO(DSN_POSTGRES, DB_USER_POSTGRES,DB_PASS_POSTGRES);
+                     self::$pdo= new PDO(DSN_RAILWAY, DB_USER_RAILWAY,DB_PASS_RAILWAY);
                      self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 }
                 return self::$pdo;
